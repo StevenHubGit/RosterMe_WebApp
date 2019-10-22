b47e9c953c8c9fffc69b21144e8d2bc61de4ee2d
Daryl George <darylgeorge646@gmail.com>
2019-10-22 22:14:20 +1300

    testing commit

---
+++ RosterMe/RosterMe/Controllers/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RosterMe.Controllers
+{
+    public class TestClass
+    {
+    }
+}

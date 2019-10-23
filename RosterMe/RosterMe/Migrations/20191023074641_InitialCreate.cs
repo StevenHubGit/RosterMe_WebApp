cae8e7222b25bafff0ed72d5461ab31562ab720d
Daryl George <darylgeorge646@gmail.com>
2019-10-23 21:43:40 +1300

    ShiftsController edited

---
+++ RosterMe/RosterMe/Migrations/20191023074641_InitialCreate.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RosterMe.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Timesheets_Employees_EmployeeId",
+                table: "Timesheets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Timesheets_Shift_ShiftId",
+                table: "Timesheets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Timesheets",
+                table: "Timesheets");
+
+            migrationBuilder.RenameTable(
+                name: "Timesheets",
+                newName: "Timesheet");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Timesheets_ShiftId",
+                table: "Timesheet",
+                newName: "IX_Timesheet_ShiftId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Timesheets_EmployeeId",
+                table: "Timesheet",
+                newName: "IX_Timesheet_EmployeeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Timesheet",
+                table: "Timesheet",
+                column: "AttendanceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Timesheet_Employees_EmployeeId",
+                table: "Timesheet",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Timesheet_Shift_ShiftId",
+                table: "Timesheet",
+                column: "ShiftId",
+                principalTable: "Shift",
+                principalColumn: "ShiftId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Timesheet_Employees_EmployeeId",
+                table: "Timesheet");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Timesheet_Shift_ShiftId",
+                table: "Timesheet");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Timesheet",
+                table: "Timesheet");
+
+            migrationBuilder.RenameTable(
+                name: "Timesheet",
+                newName: "Timesheets");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Timesheet_ShiftId",
+                table: "Timesheets",
+                newName: "IX_Timesheets_ShiftId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Timesheet_EmployeeId",
+                table: "Timesheets",
+                newName: "IX_Timesheets_EmployeeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Timesheets",
+                table: "Timesheets",
+                column: "AttendanceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Timesheets_Employees_EmployeeId",
+                table: "Timesheets",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Timesheets_Shift_ShiftId",
+                table: "Timesheets",
+                column: "ShiftId",
+                principalTable: "Shift",
+                principalColumn: "ShiftId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- RosterMe/RosterMe/Models/Entities/Shift.cs
+++ RosterMe/RosterMe/Models/Entities/Shift.cs
@@ -12,18 +12,28 @@ namespace RosterMe.Models.Entities
         private static String LOG_TAG = "Shift Model class message";
 
         //Class attributes
+        [Display(Name = "ID")]
         public int ShiftId { get; set; }
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Display(Name = "Shift Name")]
+
         public String ShiftName { get; set; }
 
+        
         [DataType(DataType.Time)]
+        [Display(Name = "Start Time")]
+
         public DateTime StartTime { get; set; }
 
         [DataType(DataType.Time)]
+        [Display(Name = "Finish Time")]
+
         public DateTime FinishTime { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+
         public DateTime ShftCreatedDate { get; set; }
     }
 }
